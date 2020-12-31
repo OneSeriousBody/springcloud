463045ad788e3444a44f7b866a3f365097e8ad63
OneSeriousBody <fadeprogramerwzt@163.com>
2020-12-31 16:55:32 +0800

    分布式配置客户端

---
+++ springcloud-config-client-3355/src/main/java/com/coderwzt/springcloud/ConfigClient.java
@@ -0,0 +1,15 @@
+package com.coderwzt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author CoderWZT
+ * @Create on 2020/12/31.
+ */
+@SpringBootApplication
+public class ConfigClient {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClient.class,args);
+  }
+}
