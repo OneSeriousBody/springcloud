6db52d6180c69cbfeb9b68a78144f46dc44b66bf
OneSeriousBody <fadeprogramerwzt@163.com>
2020-12-31 16:02:23 +0800

    服务降级

--- springcloud-api/src/main/java/com/coderwzt/springcloud/service/DeptClientServiceFallBackFactory.java
+++ springcloud-api/src/main/java/com/coderwzt/springcloud/service/DeptClientServiceFallBackFactory.java
@@ -24,6 +24,7 @@ public class DeptClientServiceFallBackFactory implements FallbackFactory {
             .setDname("id=>" + id + "没有对应的信息，客户端提供了降级的信息，这个服务现在已经被关闭")
             .setDb_source("没有数据~");
       }
+
       @Override
       public List<Dept> queryAll() {
         return null;
--- springcloud-consumer-hystrix-dashboard/src/main/java/com/coderwzt/springcloud/DeptConsumerDashboard_9001.java
+++ springcloud-consumer-hystrix-dashboard/src/main/java/com/coderwzt/springcloud/DeptConsumerDashboard_9001.java
@@ -1,8 +1,11 @@
 package com.coderwzt.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Author CoderWZT
@@ -16,5 +19,6 @@ public class DeptConsumerDashboard_9001 {
   public static void main(String[] args) {
     SpringApplication.run(DeptConsumerDashboard_9001.class,args);
   }
+
 
 }
